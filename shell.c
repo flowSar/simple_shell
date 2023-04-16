99e4013a02d21b518e33c5a9b8bc6c2a41b5258e
flowsar <brahim.sar151@gmail.com>
2023-04-16 15:27:25 +0000

    simple shell

---
+++ shell.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+char *concatenate(char *str, int len);
+
+/**
+ * main- entry function
+ * Return: 0 if success and 1 if fails .
+ */
+
+int main(void)
+{
+	char *command_line = malloc(3);
+	ssize_t command_len = 0;
+	ssize_t len;
+
+	printf("$");
+	len = getline(&command_line, &command_len, stdin);
+	command_line = concatenate(command_line, len);
+
+	char *args[] = {NULL};
+	char *envp[] = {NULL};
+
+	if (execve(command_line, args, envp) == -1) 
+	{
+		perror("./shell");
+		exit(1);
+	}
+	return (0);
+}
+/**
+ * concatenate -function that concatenate two strings.
+ * @str: input.
+ * @len : length of str.
+ * Return: string .
+ */
+
+char *concatenate(char *str, int len)
+{
+	char *path = "/bin/";
+	char *fulle_path = malloc(5 + len);
+	int i = 0, p = 0;
+
+	while (path[i] != '\0')
+	{
+		fulle_path[i] = path[i];
+		i++;
+	}
+	p = i;
+	i = 0;
+	while (str[i + 1] != '\0')
+	{
+
+		fulle_path[p] = str[i];
+		i++;
+		p++;
+	}
+	return (fulle_path);
+}
