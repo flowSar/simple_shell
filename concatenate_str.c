37fd0d94c4ae9b63f96298b0cf9a1e584ce6189a
flowsar <brahim.sar151@gmail.com>
2023-04-18 00:40:53 +0000

    update code

---
+++ concatenate_str.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * concatenate -function that concatenate two strings.
+ * @str: input string.
+ * @len : length of str.
+ * Return: string .
+ */
+char *concatenate(char *str, int len)
+{
+	char *path = "/bin/";
+	char *full_path = malloc(5 + len);
+	int i = 0, p = 0;
+
+	while (path[i] != '\0')
+	{
+		full_path[i] = path[i];
+		i++;
+	}
+	p = i;
+	i = 0;
+	while (str[i + 1] != '\0')
+	{
+		full_path[p] = str[i];
+		i++;
+		p++;
+	}
+	return (full_path);
+}
--- shell.c
+++ shell.c
@@ -2,59 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-char *concatenate(char *str, int len);
+#include <sys/wait.h>
+#include "main.h"
 
 /**
- * main- entry function
+ * main- entry function.
  * Return: 0 if success and 1 if fails .
  */
 
 int main(void)
 {
 	char *command_line = malloc(3);
-	ssize_t command_len = 0;
-	ssize_t len;
-
-	printf("$");
-	len = getline(&command_line, &command_len, stdin);
-	command_line = concatenate(command_line, len);
-
 	char *args[] = {NULL};
 	char *envp[] = {NULL};
+	unsigned long int command_len = 0;
+	ssize_t len;
+	pid_t pid = fork();
 
-	if (execve(command_line, args, envp) == -1) 
+	if (pid == 0)
 	{
-		perror("./shell");
-		exit(1);
+		printf("$");
+		len = getline(&command_line, &command_len, stdin);
+		command_line = concatenate(command_line, len);
+		if (execve(command_line, args, envp) == -1)
+		{
+			perror("./shell");
+			exit(1);
+		}
 	}
+	else
+	{
+		int status = 0;
+
+		waitpid(pid, &status, 0);
+
+		if (execve("./shell", args, envp) == -1)
+		{
+			perror("./shell");
+			exit(1);
+		}
+	}
+	free(command_line);
 	return (0);
-}
-/**
- * concatenate -function that concatenate two strings.
- * @str: input.
- * @len : length of str.
- * Return: string .
- */
-
-char *concatenate(char *str, int len)
-{
-	char *path = "/bin/";
-	char *full_path = malloc(5 + len);
-	int i = 0, p = 0;
-
-	while (path[i] != '\0')
-	{
-		full_path[i] = path[i];
-		i++;
-	}
-	p = i;
-	i = 0;
-	while (str[i + 1] != '\0')
-	{
-
-		full_path[p] = str[i];
-		i++;
-		p++;
-	}
-	return (full_path);
 }
